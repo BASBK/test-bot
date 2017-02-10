b1d6834488b993d13c7eafd93dd8856f8cf22eb7
BASBK <basbkk1@gmail.com>
2017-02-10 13:22:44 +0300

    procfile

--- main2.py
+++ main2.py
@@ -22,7 +22,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url="https://some-random-bot.herokuapp.com/bot")
+    bot.set_webhook(url="https://some-random-bot.herokuapp.com/")
     return "!", 200
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
